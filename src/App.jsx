517b7a7faba284b9918275650a1a3b7246831bfa
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-07-23 06:42:43 +0300

    first commit redux in class starts

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import AppRouter from './router/AppRouter';
+
+function App() {
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
+}
+// API_KEY=d2db4dd3ee6d4dd5afb82c78a6c75964
+export default App;
